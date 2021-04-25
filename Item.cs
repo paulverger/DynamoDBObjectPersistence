5b1b5973797035b273ea739e236a495930c64f0c
paulverger <paulverger@gmail.com>
2021-04-25 07:32:48 -0700

    Add project files.

---
+++ Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace DynamoDBObjectPersistence
+{
+	[DynamoDBTable("OldMovies")]
+	class Item
+	{
+		[DynamoDBHashKey]
+		public string MovieName { get; set; }
+		public string Stars { get; set; }
+		public int YearMade { get; set; }
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace DynamoDBObjectPersistence
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			// Add an item
+			var client = new AmazonDynamoDBClient();
+			var context = new DynamoDBContext(client);
+			var item = new Item
+			{
+				MovieName = "Breakfast at Tiffany's",
+				Stars = "Audrey Hepburn, George Peppard",
+				YearMade = 1961
+			};
+
+			context.SaveAsync(item).Wait();
+
+			// Read item
+			Item retrievedItem = context.LoadAsync<Item>("Breakfast at Tiffany's").Result;
+			Console.WriteLine("Movie is {0}, made in {1}, starring {2}",
+				retrievedItem.MovieName,
+				retrievedItem.YearMade, 
+				retrievedItem.Stars);
+
+			Console.ReadLine();
+		}
+	}
+}
